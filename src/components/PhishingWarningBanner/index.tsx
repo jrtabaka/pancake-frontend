1259ac68e42e0b3501f6243cbbc65193261e5112
bantuswap <jrx5new@gmail.com>
2022-02-27 18:16:03 -0500

    Update index.tsx

--- src/components/PhishingWarningBanner/index.tsx
+++ src/components/PhishingWarningBanner/index.tsx
@@ -10,7 +10,7 @@ const Container = styled(Flex)`
   padding: 12px;
   align-items: center;
   background: linear-gradient(0deg, rgba(39, 38, 44, 0.4), rgba(39, 38, 44, 0.4)),
-    linear-gradient(180deg, #8051d6 0%, #492286 100%);
+    linear-gradient(180deg, #f8f8ff 0%, #32cd32 100%);
   ${({ theme }) => theme.mediaQueries.md} {
     padding: 0px;
     background: linear-gradient(180deg, #8051d6 0%, #492286 100%);
@@ -44,8 +44,8 @@ const PhishingWarningBanner: React.FC = () => {
   const { t } = useTranslation()
   const [, hideBanner] = usePhishingBannerManager()
   const { isMobile, isMd } = useMatchBreakpoints()
-  const warningText = t("please make sure you're visiting https://pancakeswap.finance - check the URL carefully.")
-  const warningTextAsParts = warningText.split(/(https:\/\/pancakeswap.finance)/g)
+  const warningText = t("please make sure you're visiting https://bantustation.org - check the URL carefully.")
+  const warningTextAsParts = warningText.split(/(https:\/\/bantustation.org)/g)
   const warningTextComponent = (
     <>
       <Text as="span" color="warning" small bold textTransform="uppercase">
@@ -57,8 +57,8 @@ const PhishingWarningBanner: React.FC = () => {
           key={i}
           small
           as="span"
-          bold={text === 'https://pancakeswap.finance'}
-          color={text === 'https://pancakeswap.finance' ? '#FFFFFF' : '#BDC2C4'}
+          bold={text === 'https://bantustation.org'}
+          color={text === 'https://bantustation.org' ? '#FFFFFF' : '#BDC2C4'}
         >
           {text}
         </Text>
